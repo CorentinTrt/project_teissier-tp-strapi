11fd1fec1b2500065cc3514010c5eec730d2406b
CorentinTrt <corentintruffaut@gmail.com>
2025-01-23 06:37:32 -0500

    feat: add pages data

---
+++ src/api/page-realisation/routes/page-realisation.ts
@@ -0,0 +1,7 @@
+/**
+ * page-realisation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-realisation.page-realisation');
---
+++ src/api/page-realisation/services/page-realisation.ts
@@ -0,0 +1,7 @@
+/**
+ * page-realisation service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::page-realisation.page-realisation');
