3eb6ca59664bc83bfb41675aade598f763ecf9e0
CorentinTrt <corentintruffaut@gmail.com>
2025-01-24 07:47:35 -0500

    feat: add plugin to upload files on Cloud Storage Bucket on production

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    const BUCKET_NAME = env("BUCKET_NAME");
+    return {
+      upload: {
+        config: {
+          provider:
+            "@strapi-community/strapi-provider-upload-google-cloud-storage",
+          providerOptions: {
+            bucketName: BUCKET_NAME,
+            publicFiles: true,
+            uniform: true,
+            basePath: "",
+          },
+        },
+      },
+    };
+  }
+};
